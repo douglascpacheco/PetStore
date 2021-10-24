875e303d32b8aa307f1bb08319da8f12ccf266e9
douglas.cpacheco <douglas.cpacheco@gmail.com>
2021-10-23 22:45:10 -0300

    Finalizado o scrip excluirPet (Delete)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -62,5 +62,39 @@ public void incluirPet() throws IOException {
         ;
         System.out.println("O Token é "+ token);
     }
+    @Test(priority = 3)
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson("db/pet2.json");
 
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .put(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Cassio"))
+                .body("status", is("sold"))
+
+        ;
+    }
+    @Test(priority = 4)
+    public void excluirPet(){
+        String petId = "1608202112";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is (200))
+                .body("type", is("unknown"))
+                .body("message", is(petId))
+        ;
+    }
 }
